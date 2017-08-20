d73dc46c277d1c1c496dfd537289c4a4ec3d46df
Chris Volkernick <cvolkern@gmail.com>
2017-08-19 23:54:33 -0400

    Implemented authentication & roles.

--- REMS/Controllers/HomeController.cs
+++ REMS/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using REMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace REMS.Controllers
 {
@@ -25,6 +29,25 @@ namespace REMS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private Boolean IsAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+                var s = userManager.GetRoles(user.GetUserId());
+
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
--- REMS/Startup.cs
+++ REMS/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using REMS.Models;
 
 [assembly: OwinStartupAttribute(typeof(REMS.Startup))]
 namespace REMS
@@ -9,6 +12,48 @@ namespace REMS
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();            
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "SysAdmin";
+                user.Email = "cvolkern@gmail.com";
+                string userPass = "SuiteSuccess17!";
+
+                var checkUser = userManager.Create(user, userPass);
+
+                if (checkUser.Succeeded)
+                {
+                    var createAdminResult = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Staff"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Staff";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Client"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Client";
+                roleManager.Create(role);
+            }
         }
     }
 }
